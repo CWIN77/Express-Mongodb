4dd695700a053a66942dd608999d0c9479d512b9
CWIN77 <cwstbp0426@gmail.com>
2022-11-14 21:02:34 +0900

    스케쥴러 시간을 8분으로 변경

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`Server on ${PORT} port`);
-  cron.schedule('* */10 * * * *', async () => {
+  cron.schedule('* */8 * * * *', async () => {
     const response = await fetch("https://express-mongodb-eta.vercel.app");
     const data = await response.json();
     console.log(data);
