bc2058a9e895f0f61c57f2cc5b40588f8dfdf75c
CWIN77 <cwstbp0426@gmail.com>
2022-11-15 01:01:24 +0900

    cpu수 확인 추가

--- src/home.ctrl.js
+++ src/home.ctrl.js
@@ -1,8 +1,10 @@
 const connectDB = require('../config/db');
+const { cpus } = require("os");
+const numCPUs = cpus().length;
 
 const getRes = (req, res) => {
   connectDB();
-  res.status(200).json("Called!");
+  res.status(200).json(numCPUs);
 };
 
 module.exports = { getRes };
