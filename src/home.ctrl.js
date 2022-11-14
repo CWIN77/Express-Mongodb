03ea7ca2dfb9591443869eb7d07c71595b5eb099
CWIN77 <cwstbp0426@gmail.com>
2022-11-15 02:44:26 +0900

    cluster 변경

--- src/home.ctrl.js
+++ src/home.ctrl.js
@@ -3,8 +3,8 @@
 const cluster = require("cluster");
 
 const getRes = (req, res) => {
-  res.status(200).json(cluster);
-  console.log(cluster);
+  res.status(200).json(cluster.isPrimary || cluster.isMaster);
+  console.log(cluster.isPrimary || cluster.isMaster);
 };
 
 module.exports = { getRes };
