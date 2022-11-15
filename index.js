1fb757679b6d6d721f5ae3e3a84b1570e7fade77
CWIN77 <cwstbp0426@gmail.com>
2022-11-15 16:49:46 +0900

    mongodb연결을 제거하고 테스트용 api를 추가

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const compression = require('compression')
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connectDB = require('./config/db');
-connectDB();
+// const connectDB = require('./config/db');
+// connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,11 +15,26 @@ app.use(helmet());
 app.use(cors());
 app.use(compression());
 
-const goal = require("./src/goal");
+// const goal = require("./src/goal");
 const home = require("./src/home");
 
 app.use("/", home);
-app.use("/goal", goal);
+// app.use("/goal", goal);
+
+// 성능 체크
+// npm i -g loadtest
+// loadtest http://localhost:9000/api/testcluster -n 1000 -c 100
+app.get('/api/testcluster', (req, res) => {
+  console.time('noclusterApi');
+  const base = 8;
+  let result = 0;
+  for (let i = Math.pow(base, 7); i >= 0; i--) {
+    result += i + Math.pow(i, 10);
+  };
+  console.timeEnd('noclusterApi');
+  console.log(`RESULT IS ${result} - ON PROCESS ${process.pid}`);
+  res.send(`Result number is ${result}`);
+});
 
 const PORT = process.env.PORT || 9000;
 
