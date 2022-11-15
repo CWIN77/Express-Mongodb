cb26bd6498410be1f7c980e9869c54b08733154e
CWIN77 <cwstbp0426@gmail.com>
2022-11-15 17:13:50 +0900

    서버 부하량을 절반으로 줄임

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/api/testcluster', (req, res) => {
   console.time('noclusterApi');
   const base = 8;
   let result = 0;
-  for (let i = Math.pow(base, 7) * 50; i >= 0; i--) {
+  for (let i = Math.pow(base, 7) * 20; i >= 0; i--) {
     result += i + Math.pow(i, 10);
   };
   console.timeEnd('noclusterApi');
